f6973795802356d726242924df2e19a1ef2a0a89
Utkarsh Sharma <utkarshs12121@gmail.com>
2026-01-27 21:15:51 +0530

    Remove unused packages folder (old Svelte/Node.js code)

--- packages/frontend/svelte.config.js
+++
@@ -1,8 +0,0 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-export default {
-    preprocess: vitePreprocess(),
-    compilerOptions: {
-        // Add compiler options if needed
-    }
-};
